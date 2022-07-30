2cc0b7bfe248861536fb8555cd3483a81d134a63
seonga <girl0930h@naver.com>
2022-07-30 16:28:34 +0900

    스타일 컴포넌트 적용

--- src/components/CourseGoals/CourseGoalList/CourseGoalList.js
+++ src/components/CourseGoals/CourseGoalList/CourseGoalList.js
@@ -1,21 +1,33 @@
 import React from 'react';
-
 import CourseGoalItem from '../CourseGoalItem/CourseGoalItem';
-import './CourseGoalList.css';
+import styled from 'styled-components';
+
+const GoalList = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+
+  @media (min-width: 768px) {
+    width : 900px;
+  }
+`
+
 
 const CourseGoalList = props => {
   return (
-    <ul className="goal-list">
+    <GoalList>
       {props.items.map(goal => (
         <CourseGoalItem
           key={goal.id}
           id={goal.id}
-          onDelete={props.onDeleteItem}
-        >
+          onDelete={props.onDeleteItem}>
           {goal.text}
         </CourseGoalItem>
       ))}
-    </ul>
+    </GoalList>
   );
 };
 
